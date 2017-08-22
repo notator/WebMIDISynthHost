93dbc3201ef82e7a8ec3498bde1fb8b835e23304
notator <j.ingram@netcologne.de>
2017-08-22 19:34:00 +0200

    Changes to NoteOff function
    volRelease and modReleae now limited to max 0.05sec.
    Using gain.setTargetAtTime() to end envelopes instead of
    gain.linearRampToValue()

--- residentSf2Synth/soundFontSynthNote.js
+++ residentSf2Synth/soundFontSynthNote.js
@@ -19,46 +19,46 @@ WebMIDI.namespace('WebMIDI.soundFontSynthNote');
 
 WebMIDI.soundFontSynthNote = (function()
 {
-	"use strict";
-	var
+    "use strict";
+    var
 	SoundFontSynthNote = function(ctx, gainMaster, instrument)
 	{
-		this.ctx = ctx;
-		this.gainMaster = gainMaster;
-		this.instrument = instrument;
-		this.channel = instrument.channel;
-		this.key = instrument.key;
-		this.velocity = instrument.velocity;
-		this.buffer = instrument.sample;
-		this.playbackRate = instrument.basePlaybackRate;
-		this.sampleRate = instrument.sampleRate;
-		this.volume = instrument.volume;
-		this.panpot = instrument.panpot;
-		this.pitchBend = instrument.pitchBend;
-		this.pitchBendSensitivity = instrument.pitchBendSensitivity;
-		this.modEnvToPitch = instrument.modEnvToPitch;
+	    this.ctx = ctx;
+	    this.gainMaster = gainMaster;
+	    this.instrument = instrument;
+	    this.channel = instrument.channel;
+	    this.key = instrument.key;
+	    this.velocity = instrument.velocity;
+	    this.buffer = instrument.sample;
+	    this.playbackRate = instrument.basePlaybackRate;
+	    this.sampleRate = instrument.sampleRate;
+	    this.volume = instrument.volume;
+	    this.panpot = instrument.panpot;
+	    this.pitchBend = instrument.pitchBend;
+	    this.pitchBendSensitivity = instrument.pitchBendSensitivity;
+	    this.modEnvToPitch = instrument.modEnvToPitch;
 
-		// state
-		this.startTime = ctx.currentTime;
-		this.computedPlaybackRate = this.playbackRate;
+	    // state
+	    this.startTime = ctx.currentTime;
+	    this.computedPlaybackRate = this.playbackRate;
 
-		// audio node
-		this.audioBuffer = null;
-		this.bufferSource = null;
-		this.panner = null;
-		this.gainOutput = null;
+	    // audio node
+	    this.audioBuffer = null;
+	    this.bufferSource = null;
+	    this.panner = null;
+	    this.gainOutput = null;
 
-		//console.log(instrument.modAttack, instrument.modDecay, instrument.modSustain, instrument.modRelease);	
+	    //console.log(instrument.modAttack, instrument.modDecay, instrument.modSustain, instrument.modRelease);	
 	},
 
 	API =
 	{
-		SoundFontSynthNote: SoundFontSynthNote // constructor
+	    SoundFontSynthNote: SoundFontSynthNote // constructor
 	};
 
-	SoundFontSynthNote.prototype.noteOn = function()
-	{
-		var
+    SoundFontSynthNote.prototype.noteOn = function()
+    {
+        var
 		buffer, channelData, bufferSource, filter, panner,
 		output, outputGain, baseFreq, peekFreq, sustainFreq,
 		ctx = this.ctx,
@@ -74,162 +74,171 @@ WebMIDI.soundFontSynthNote = (function()
 		loopEnd = 0,
 		startTime = instrument.start / this.sampleRate;
 
-		function amountToFreq(val)
-		{
-			return Math.pow(2, (val - 6900) / 1200) * 440;
-		}
+        function amountToFreq(val)
+        {
+            return Math.pow(2, (val - 6900) / 1200) * 440;
+        }
 
-		if(instrument.doLoop === true)
-		{
-			loopStart = instrument.loopStart / this.sampleRate;
-			loopEnd = instrument.loopEnd / this.sampleRate;
-		}
-		sample = sample.subarray(0, sample.length + instrument.end);
-		this.audioBuffer = ctx.createBuffer(1, sample.length, this.sampleRate);
-		buffer = this.audioBuffer;
-		channelData = buffer.getChannelData(0);
-		channelData.set(sample);
+        if(instrument.doLoop === true)
+        {
+            loopStart = instrument.loopStart / this.sampleRate;
+            loopEnd = instrument.loopEnd / this.sampleRate;
+        }
+        sample = sample.subarray(0, sample.length + instrument.end);
+        this.audioBuffer = ctx.createBuffer(1, sample.length, this.sampleRate);
+        buffer = this.audioBuffer;
+        channelData = buffer.getChannelData(0);
+        channelData.set(sample);
 
-		// buffer source
-		this.bufferSource = ctx.createBufferSource();
-		bufferSource = this.bufferSource;
-		bufferSource.buffer = buffer;
-		/* ji begin changes December 2015 */
-		// This line was originally:
-		//    bufferSource.loop = (this.channel !== 9);
-		bufferSource.loop = (this.channel !== 9) && (instrument.doLoop === true);
-		/* ji end changes December 2015 */
-		bufferSource.loopStart = loopStart;
-		bufferSource.loopEnd = loopEnd;
-		this.updatePitchBend(this.pitchBend);
+        // buffer source
+        this.bufferSource = ctx.createBufferSource();
+        bufferSource = this.bufferSource;
+        bufferSource.buffer = buffer;
+        /* ji begin changes December 2015 */
+        // This line was originally:
+        //    bufferSource.loop = (this.channel !== 9);
+        bufferSource.loop = (this.channel !== 9) && (instrument.doLoop === true);
+        /* ji end changes December 2015 */
+        bufferSource.loopStart = loopStart;
+        bufferSource.loopEnd = loopEnd;
+        this.updatePitchBend(this.pitchBend);
 
-		// audio node
-		this.panner = ctx.createPanner();
-		panner = this.panner;
-		this.gainOutput = ctx.createGain();
-		output = this.gainOutput;
-		outputGain = output.gain;
+        // audio node
+        this.panner = ctx.createPanner();
+        panner = this.panner;
+        this.gainOutput = ctx.createGain();
+        output = this.gainOutput;
+        outputGain = output.gain;
 
-		// filter
-		this.filter = ctx.createBiquadFilter();
-		filter = this.filter;
-		filter.type = 'lowpass';
+        // filter
+        this.filter = ctx.createBiquadFilter();
+        filter = this.filter;
+        filter.type = 'lowpass';
 
-		// panpot
-		panner.panningModel = 'HRTF';
-		panner.setPosition(
+        // panpot
+        panner.panningModel = 'HRTF';
+        panner.setPosition(
 		  Math.sin(this.panpot * Math.PI / 2),
 		  0,
 		  Math.cos(this.panpot * Math.PI / 2)
 		);
 
-		//---------------------------------------------------------------------------
-		// Attack, Decay, Sustain
-		//---------------------------------------------------------------------------
-		outputGain.setValueAtTime(0, now);
+        //---------------------------------------------------------------------------
+        // Attack, Decay, Sustain
+        //---------------------------------------------------------------------------
+        outputGain.setValueAtTime(0, now);
 
-	    // begin original gree
-		//outputGain.linearRampToValueAtTime(this.volume * (this.velocity / 127), volAttack);
-		//outputGain.linearRampToValueAtTime(this.volume * (1 - instrument.volSustain), volDecay);
-	    // end original gree
+        // begin original gree
+        //outputGain.linearRampToValueAtTime(this.volume * (this.velocity / 127), volAttack);
+        //outputGain.linearRampToValueAtTime(this.volume * (1 - instrument.volSustain), volDecay);
+        // end original gree
 
-	    // begin ji changes August 2017
+        // begin ji changes August 2017
         // volLevel is a new variable, defined in the vars above.
-		outputGain.linearRampToValueAtTime(volLevel, volAttack);
-	    // For the following line see https://github.com/notator/WebMIDISynthHost/issues/29
-	    // Thanks @timjrd !
-	    // ji -- the instrument.volSustain attribute is a level parameter, not like the other
-	    // instrument.vol... attributes (which are time values).
-		outputGain.linearRampToValueAtTime(volLevel * (1 - instrument.volSustain), volDecay);
+        outputGain.linearRampToValueAtTime(volLevel, volAttack);
+        // For the following line see https://github.com/notator/WebMIDISynthHost/issues/29
+        // Thanks @timjrd !
+        // ji -- the instrument.volSustain attribute is a level parameter, not like the other
+        // instrument.vol... attributes (which are time values).
+        outputGain.linearRampToValueAtTime(volLevel * (1 - instrument.volSustain), volDecay);
         // end ji changes August 2017
 
-		// begin ji changes November 2015.
-		// The following original line was a (deliberate, forgotten?) bug that threw an out-of-range
-		// exception when instrument['initialFilterQ'] > 0:
-		//     filter.Q.setValueAtTime(instrument['initialFilterQ'] * Math.pow(10, 200), now);
-		// The following line seems to work, but is it realy correct?
-		filter.Q.setValueAtTime(instrument.initialFilterQ, now);
-		// end ji ji changes November 2015
+        // begin ji changes November 2015.
+        // The following original line was a (deliberate, forgotten?) bug that threw an out-of-range
+        // exception when instrument['initialFilterQ'] > 0:
+        //     filter.Q.setValueAtTime(instrument['initialFilterQ'] * Math.pow(10, 200), now);
+        // The following line seems to work, but is it realy correct?
+        filter.Q.setValueAtTime(instrument.initialFilterQ, now);
+        // end ji ji changes November 2015
 
-		baseFreq = amountToFreq(instrument.initialFilterFc);
-		peekFreq = amountToFreq(instrument.initialFilterFc + instrument.modEnvToFilterFc);
-		sustainFreq = baseFreq + (peekFreq - baseFreq) * (1 - instrument.modSustain);
-		filter.frequency.setValueAtTime(baseFreq, now);
-		filter.frequency.linearRampToValueAtTime(peekFreq, modAttack);
-		filter.frequency.linearRampToValueAtTime(sustainFreq, modDecay);
+        baseFreq = amountToFreq(instrument.initialFilterFc);
+        peekFreq = amountToFreq(instrument.initialFilterFc + instrument.modEnvToFilterFc);
+        sustainFreq = baseFreq + (peekFreq - baseFreq) * (1 - instrument.modSustain);
+        filter.frequency.setValueAtTime(baseFreq, now);
+        filter.frequency.linearRampToValueAtTime(peekFreq, modAttack);
+        filter.frequency.linearRampToValueAtTime(sustainFreq, modDecay);
 
-		// connect
-		bufferSource.connect(filter);
-		filter.connect(panner);
-		panner.connect(output);
-		output.connect(this.gainMaster);
+        // connect
+        bufferSource.connect(filter);
+        filter.connect(panner);
+        panner.connect(output);
+        output.connect(this.gainMaster);
 
-		// fire
-		bufferSource.start(0, startTime);
-	};
+        // fire
+        bufferSource.start(0, startTime);
+    };
 
-	SoundFontSynthNote.prototype.noteOff = function()
-	{
-		var
+    SoundFontSynthNote.prototype.noteOff = function()
+    {
+        var
 		instrument = this.instrument,
 		bufferSource = this.bufferSource,
 		output = this.gainOutput,
 		now = this.ctx.currentTime,
+
 		// begin gree
 		//   volEndTime = now + instrument.volRelease,
 		//   modEndTime = now + instrument.modRelease;
 		// end gree
+
 		// begin ji
 		// instrument.volRelease is 3.08 in preset 0 (grand piano) in the Arachno font.   
 		// It cannot be the case that a piano note only stops 3.08 seconds after a
-		// noteOff arrives. Multiplying these parameters by 0.1 sounds more realistic...
-		volEndTime = now + (instrument.volRelease * 0.1),
-		modEndTime = now + (instrument.modRelease * 0.1);
-		// end ji
+		// noteOff arrives.
+        // The following line limits the value to 0.05 seconds.
+        // This is a temporary kludge, pending the proper solution to the problem...
+        volRelease = (instrument.volRelease > 0.05) ? 0.05 : instrument.volRelease,
+        modRelease = (instrument.modRelease > 0.05) ? 0.05 : instrument.modRelease,
+		volEndTime = now + volRelease,
+		modEndTime = now + modRelease;
+        // end ji
 
-		if(!this.audioBuffer)
-		{
-			return;
-		}
+        if(!this.audioBuffer)
+        {
+            return;
+        }
 
-		//---------------------------------------------------------------------------
-		// Release
-		//---------------------------------------------------------------------------
-		// begin original gree
-		//output.gain.cancelScheduledValues(0);
-		//output.gain.linearRampToValueAtTime(0, volEndTime);
-		//bufferSource.playbackRate.cancelScheduledValues(0);
-		//bufferSource.playbackRate.linearRampToValueAtTime(this.computedPlaybackRate, modEndTime);
-		// end original gree
+        //---------------------------------------------------------------------------
+        // Release
+        //---------------------------------------------------------------------------
+        // begin original gree
+        //output.gain.cancelScheduledValues(0);
+        //output.gain.linearRampToValueAtTime(0, volEndTime);
+        //bufferSource.playbackRate.cancelScheduledValues(0);
+        //bufferSource.playbackRate.linearRampToValueAtTime(this.computedPlaybackRate, modEndTime);
+        // end original gree
 
-		// begin ji
-		output.gain.cancelScheduledValues(volEndTime);
-		output.gain.linearRampToValueAtTime(0, volEndTime);
-		bufferSource.playbackRate.cancelScheduledValues(modEndTime);
-		bufferSource.playbackRate.linearRampToValueAtTime(this.computedPlaybackRate, modEndTime);
-		// end ji
+        // begin ji
+        // latest changes:
+        // 1. use setTargetAtTime() instead of linearRampToValueAtTime(0, volEndTime). (Suggested by Timothée Jourde on GitHub).
+        // 2. call cancelScheduledValues(...) _after_ setting the envelopes, not before.
+        output.gain.setTargetAtTime(0, now, volRelease);
+        output.gain.cancelScheduledValues(volEndTime);
+        bufferSource.playbackRate.linearRampToValueAtTime(this.computedPlaybackRate, modEndTime);
+        bufferSource.playbackRate.cancelScheduledValues(modEndTime);
+        // end ji
 
-		bufferSource.loop = false;
-		bufferSource.stop(volEndTime);
+        bufferSource.loop = false;
+        bufferSource.stop(volEndTime);
 
-		// disconnect
-		setTimeout(
+        // disconnect
+        setTimeout(
 		  (function(note)
-			{
-		  		return function()
-		  		{
-		  			note.bufferSource.disconnect(0);
-		  			note.panner.disconnect(0);
-		  			note.gainOutput.disconnect(0);
-		  		};
-			}(this)),
+		  {
+		      return function()
+		      {
+		          note.bufferSource.disconnect(0);
+		          note.panner.disconnect(0);
+		          note.gainOutput.disconnect(0);
+		      };
+		  }(this)),
 		  instrument.volRelease * 1000
 		);
-	};
+    };
 
-	SoundFontSynthNote.prototype.schedulePlaybackRate = function()
-	{
-		var
+    SoundFontSynthNote.prototype.schedulePlaybackRate = function()
+    {
+        var
 		playbackRate = this.bufferSource.playbackRate,
 		computed = this.computedPlaybackRate,
 		start = this.startTime,
@@ -238,15 +247,15 @@ WebMIDI.soundFontSynthNote = (function()
 		modDecay = modAttack + instrument.modDecay,
 		peekPitch = computed * Math.pow(Math.pow(2, 1 / 12), this.modEnvToPitch * this.instrument.scaleTuning);
 
-		playbackRate.cancelScheduledValues(0);
-		playbackRate.setValueAtTime(computed, start);
-		playbackRate.linearRampToValueAtTime(peekPitch, modAttack);
-		playbackRate.linearRampToValueAtTime(computed + (peekPitch - computed) * (1 - instrument.modSustain), modDecay);
-	};
+        playbackRate.cancelScheduledValues(0);
+        playbackRate.setValueAtTime(computed, start);
+        playbackRate.linearRampToValueAtTime(peekPitch, modAttack);
+        playbackRate.linearRampToValueAtTime(computed + (peekPitch - computed) * (1 - instrument.modSustain), modDecay);
+    };
 
-	SoundFontSynthNote.prototype.updatePitchBend = function(pitchBend)
-	{
-		this.computedPlaybackRate = this.playbackRate * Math.pow(
+    SoundFontSynthNote.prototype.updatePitchBend = function(pitchBend)
+    {
+        this.computedPlaybackRate = this.playbackRate * Math.pow(
 		  Math.pow(2, 1 / 12),
 		  (
 			this.pitchBendSensitivity * (
@@ -254,9 +263,9 @@ WebMIDI.soundFontSynthNote = (function()
 			)
 		  ) * this.instrument.scaleTuning
 		);
-		this.schedulePlaybackRate();
-	};
+        this.schedulePlaybackRate();
+    };
 
-	return API;
+    return API;
 
 }());
