19de07927974fdc89d765bfe1494cf0f17fc98af
notator <j.ingram@netcologne.de>
2018-12-14 16:21:59 +0100

    Moved the call to getAudioContext() from the constructor to the open() function.
    This is to comply with Chrome's autoplay policy (December 2018) in which the audioContext object has to be created after the user has interacted with the site. See:
    https://developers.google.com/web/updates/2017/09/autoplay-policy-changes#webaudio
    Also moved the code in init() after the new code in open(), and deleted the init() function.

--- residentSf2Synth/residentSf2Synth.js
+++ residentSf2Synth/residentSf2Synth.js
@@ -23,136 +23,148 @@ WebMIDI.namespace('WebMIDI.residentSf2Synth');
 
 WebMIDI.residentSf2Synth = (function(window)
 {
-    "use strict";
+	"use strict";
 
 	var
-    /*********************************************************
-	 * ji -- November 2015
-	 * These variables were originally in the gree code.
-	 * I have removed all references to the original gree GUI
-	 * (i.e. the HTML <table> and the instrument names).
-	 *********************************************************/
-	bankIndex = 0,
-	bankSet,
-	ctx, // set in constructor
-	gainMaster,  // set in constructor (ji)
+		/*********************************************************
+		 * ji -- November 2015
+		 * These variables were originally in the gree code.
+		 * I have removed all references to the original gree GUI
+		 * (i.e. the HTML <table> and the instrument names).
+		 *********************************************************/
+		bankIndex = 0,
+		bankSet,
+		ctx, // set in constructor
+		gainMaster,  // set in constructor (ji)
 
-	/** ji begin compressor commented out because unused November 2015 */
-	/** @type {DynamicsCompressorNode} */
-	// compressor,
-	/** ji end compressor commented out because unused November 2015 */
+		/** ji begin compressor commented out because unused November 2015 */
+		/** @type {DynamicsCompressorNode} */
+		// compressor,
+		/** ji end compressor commented out because unused November 2015 */
 
-	bufSrc,  // set in constructor (ji)
-	channelInstrument =
-	  [0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 10, 11, 12, 13, 14, 15],
-	channelVolume =
-	  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	channelPanpot =
-	  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	channelPitchBend =
-	  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	channelRegisteredParameterCoarse =
-	  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    channelDataEntryCoarse =
-	  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-	currentNoteOns = [],
-	baseVolume = 1 / 0x8000,
+		bufSrc,  // set in constructor (ji)
+		channelInstrument =
+			[0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 10, 11, 12, 13, 14, 15],
+		channelVolume =
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		channelPanpot =
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		channelPitchBend =
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		channelRegisteredParameterCoarse =
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		channelDataEntryCoarse =
+			[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+		currentNoteOns = [],
+		baseVolume = 1 / 0x8000,
 
-	/*  end of gree variables  ****************************************/
-	/******************************************************************/
+		/*  end of gree variables  ****************************************/
+		/******************************************************************/
 
-	getAudioContext = function()
-	{
-		var AudioContext = (window.AudioContext || window.webkitAudioContext);
-
-		return new AudioContext();
-	},
-
-	CMD = WebMIDI.constants.COMMAND,
-	CTL = WebMIDI.constants.CONTROL,
-
-	// The commands and controls arrays are part of a standard WebMIDI synth's interface.
-	commands =
-	[
-		// The name strings and defaultValues (if any) for these are defined in WebMIDI.constants
-		CMD.NOTE_OFF,
-		CMD.NOTE_ON,
-		// CMD.AFTERTOUCH is not defined,
-		CMD.CONTROL_CHANGE,
-		CMD.PATCH,
-		// CMD.CHANNEL_PRESSURE is not defined,
-		CMD.PITCHWHEEL
-	],
-
-	controls =
-	[
-		// standard 3-byte controllers.
-		// The name strings and defaultValues for these are defined in WebMIDI.constants
-		CTL.BANK,
-		CTL.VOLUME,
-		CTL.PAN,
-
-        // REGISTERED_PARAMETER_COARSE can be set by client software, but it should never be set to anything except 0.
-        // If the value is set to anything other than 0, attempts to retrieve pitchBendSensitivity will fail.
-        // These synths don't use REGISTERED_PARAMETER_COARSE for anything else.
-        // The REGISTERED_PARAMETER_COARSE control has been deliberately omitted from this synth's UI in the WebMIDISynthHost application. 
-        CTL.REGISTERED_PARAMETER_COARSE,
-        CTL.DATA_ENTRY_COARSE,
-
-		// standard 2-byte controllers.
-		// The name strings for these are defined in WebMIDI.constants
-		CTL.ALL_CONTROLLERS_OFF,
-		CTL.ALL_SOUND_OFF
-	],
-
-	ResidentSf2Synth = function()
-	{
-		var i;
-
-		if(!(this instanceof ResidentSf2Synth))
+		getAudioContext = function()
 		{
-			return new ResidentSf2Synth();
-		}
+			var AudioContext = (window.AudioContext || window.webkitAudioContext);
 
-		// WebMIDIAPI §4.6 -- MIDIPort interface
-		// See https://github.com/notator/WebMIDISynthHost/issues/23
-		// and https://github.com/notator/WebMIDISynthHost/issues/24
-		Object.defineProperty(this, "id", { value: "Sf2Synth01", writable: false });
-		Object.defineProperty(this, "manufacturer", { value: "gree & ji", writable: false });
-		Object.defineProperty(this, "name", { value: "ResidentSf2Synth", writable: false });
-		Object.defineProperty(this, "type", { value: "output", writable: false });
-		Object.defineProperty(this, "version", { value: "1", writable: false });
-		Object.defineProperty(this, "ondisconnect", { value: null, writable: false }); // Do we need this at all? Is it correct to set it to null?
+			return new AudioContext();
+		},
 
-		/*** Is this necessary? See https://github.com/WebAudio/web-midi-api/issues/110 ***/
-		/*** See also: disconnect() function below ***/
-		Object.defineProperty(this, "removable", { value: true, writable: false });
+		CMD = WebMIDI.constants.COMMAND,
+		CTL = WebMIDI.constants.CONTROL,
 
-		/*** Extensions for software synths ***/
-		// The synth author's webpage hosting the synth. 
-		Object.defineProperty(this, "url", { value: "https://github.com/gree/sf2synth.js", writable: false });
-		// The commands supported by this synth (see above).
-		Object.defineProperty(this, "commands", { value: commands, writable: false });
-		// The controls supported by this synth (see above).
-		Object.defineProperty(this, "controls", { value: controls, writable: false });
-		// If isMultiChannel is false or undefined, the synth ignores the channel nibble in MIDI messages
-		Object.defineProperty(this, "isMultiChannel", { value: true, writable: false });
-		// If isPolyphonic is false or undefined, the synth can only play one note at a time
-		Object.defineProperty(this, "isPolyphonic", { value: true, writable: false });
-		// If supportsGeneralMIDI is defined, and is true, then
-		// 1. both COMMAND.PATCH and CONTROL.BANK MUST be defined.
-		// 2. the patches in bank 0 can be usefully named using GM patch names.
-		//    (GM patch names are returned by WebMIDI.constants.generalMIDIPatchName(patchIndex). )
-		// 3. when the channel index is 9, notes can be usefully named using the GM percussion names.
-		//    (GM percussion names are returned by WebMIDI.constants.generalMIDIPercussionName(noteIndex). )
-		// 4. the synth MUST define the function:
-		//        boolean patchIsAvailable(patchIndex).
-		//    On their own, conditions 1-3 do not guarantee that a particular patch can be set.
-		// 5. the synth MAY define the function:
-		//        void setSoundFont(soundFont)
-		//    It is possible for a synth to support GM without using soundfonts.
-		Object.defineProperty(this, "supportsGeneralMIDI", { value: true, writable: false });
+		// The commands and controls arrays are part of a standard WebMIDI synth's interface.
+		commands =
+			[
+				// The name strings and defaultValues (if any) for these are defined in WebMIDI.constants
+				CMD.NOTE_OFF,
+				CMD.NOTE_ON,
+				// CMD.AFTERTOUCH is not defined,
+				CMD.CONTROL_CHANGE,
+				CMD.PATCH,
+				// CMD.CHANNEL_PRESSURE is not defined,
+				CMD.PITCHWHEEL
+			],
 
+		controls =
+			[
+				// standard 3-byte controllers.
+				// The name strings and defaultValues for these are defined in WebMIDI.constants
+				CTL.BANK,
+				CTL.VOLUME,
+				CTL.PAN,
+
+				// REGISTERED_PARAMETER_COARSE can be set by client software, but it should never be set to anything except 0.
+				// If the value is set to anything other than 0, attempts to retrieve pitchBendSensitivity will fail.
+				// These synths don't use REGISTERED_PARAMETER_COARSE for anything else.
+				// The REGISTERED_PARAMETER_COARSE control has been deliberately omitted from this synth's UI in the WebMIDISynthHost application. 
+				CTL.REGISTERED_PARAMETER_COARSE,
+				CTL.DATA_ENTRY_COARSE,
+
+				// standard 2-byte controllers.
+				// The name strings for these are defined in WebMIDI.constants
+				CTL.ALL_CONTROLLERS_OFF,
+				CTL.ALL_SOUND_OFF
+			],
+
+		ResidentSf2Synth = function()
+		{
+			if(!(this instanceof ResidentSf2Synth))
+			{
+				return new ResidentSf2Synth();
+			}
+
+			// WebMIDIAPI §4.6 -- MIDIPort interface
+			// See https://github.com/notator/WebMIDISynthHost/issues/23
+			// and https://github.com/notator/WebMIDISynthHost/issues/24
+			Object.defineProperty(this, "id", { value: "Sf2Synth01", writable: false });
+			Object.defineProperty(this, "manufacturer", { value: "gree & ji", writable: false });
+			Object.defineProperty(this, "name", { value: "ResidentSf2Synth", writable: false });
+			Object.defineProperty(this, "type", { value: "output", writable: false });
+			Object.defineProperty(this, "version", { value: "1", writable: false });
+			Object.defineProperty(this, "ondisconnect", { value: null, writable: false }); // Do we need this at all? Is it correct to set it to null?
+
+			/*** Is this necessary? See https://github.com/WebAudio/web-midi-api/issues/110 ***/
+			/*** See also: disconnect() function below ***/
+			Object.defineProperty(this, "removable", { value: true, writable: false });
+
+			/*** Extensions for software synths ***/
+			// The synth author's webpage hosting the synth. 
+			Object.defineProperty(this, "url", { value: "https://github.com/gree/sf2synth.js", writable: false });
+			// The commands supported by this synth (see above).
+			Object.defineProperty(this, "commands", { value: commands, writable: false });
+			// The controls supported by this synth (see above).
+			Object.defineProperty(this, "controls", { value: controls, writable: false });
+			// If isMultiChannel is false or undefined, the synth ignores the channel nibble in MIDI messages
+			Object.defineProperty(this, "isMultiChannel", { value: true, writable: false });
+			// If isPolyphonic is false or undefined, the synth can only play one note at a time
+			Object.defineProperty(this, "isPolyphonic", { value: true, writable: false });
+			// If supportsGeneralMIDI is defined, and is true, then
+			// 1. both COMMAND.PATCH and CONTROL.BANK MUST be defined.
+			// 2. the patches in bank 0 can be usefully named using GM patch names.
+			//    (GM patch names are returned by WebMIDI.constants.generalMIDIPatchName(patchIndex). )
+			// 3. when the channel index is 9, notes can be usefully named using the GM percussion names.
+			//    (GM percussion names are returned by WebMIDI.constants.generalMIDIPercussionName(noteIndex). )
+			// 4. the synth MUST define the function:
+			//        boolean patchIsAvailable(patchIndex).
+			//    On their own, conditions 1-3 do not guarantee that a particular patch can be set.
+			// 5. the synth MAY define the function:
+			//        void setSoundFont(soundFont)
+			//    It is possible for a synth to support GM without using soundfonts.
+			Object.defineProperty(this, "supportsGeneralMIDI", { value: true, writable: false });
+		},
+
+		API =
+		{
+			ResidentSf2Synth: ResidentSf2Synth // constructor
+		};
+	// end var
+
+	// WebMIDIAPI §4.6 -- MIDIPort interface
+	// See https://github.com/notator/WebMIDISynthHost/issues/24
+	// This is called when the start button is clicked on page 1 of the Assistant Performer,
+	// and the synth is selected in the outputDeviceSelector.
+	ResidentSf2Synth.prototype.open = function()
+	{
+		// console.log("residentSf2Synth opened.");
 		ctx = getAudioContext();
 		gainMaster = ctx.createGain();
 
@@ -165,23 +177,17 @@ WebMIDI.residentSf2Synth = (function(window)
 
 		bufSrc = ctx.createBufferSource();
 
-		for(i = 0; i < 16; ++i)
+		for(let i = 0; i < 16; ++i)
 		{
 			currentNoteOns.push([]);
 		}
-	},
 
-	API =
-    {
-    	ResidentSf2Synth: ResidentSf2Synth // constructor
-    };
-	// end var
+		bufSrc.connect(gainMaster);
+		gainMaster.connect(ctx.destination);
+		bufSrc.start(0);
 
-	// WebMIDIAPI §4.6 -- MIDIPort interface
-	// See https://github.com/notator/WebMIDISynthHost/issues/24
-	ResidentSf2Synth.prototype.open = function()
-	{
-		// console.log("residentSf2Synth opened.");
+		// ji November 2015
+		this.setMasterVolume(16383);
 	};
 
 	// WebMIDIAPI §4.6 -- MIDIPort interface
@@ -196,11 +202,11 @@ WebMIDI.residentSf2Synth = (function(window)
 	ResidentSf2Synth.prototype.send = function(message, ignoredTimestamp)
 	{
 		var
-		command = message[0] & 0xF0,
-		channel = message[0] & 0xF,
-		data1 = message[1],
-		data2 = message[2],
-		that = this;
+			command = message[0] & 0xF0,
+			channel = message[0] & 0xF,
+			data1 = message[1],
+			data2 = message[2],
+			that = this;
 
 		function checkCommandExport(command)
 		{
@@ -266,8 +272,8 @@ WebMIDI.residentSf2Synth = (function(window)
 
 			function setRegisteredParameterCoarse(channel, param)
 			{
-			    checkControlExport(CTL.REGISTERED_PARAMETER_COARSE);
-			    // console.log("residentSf2Synth RegisteredParameterCoarse: channel:" + channel + " value:" + param);
+				checkControlExport(CTL.REGISTERED_PARAMETER_COARSE);
+				// console.log("residentSf2Synth RegisteredParameterCoarse: channel:" + channel + " value:" + param);
 				if(param !== 0)
 				{
 					throw "This synth only supports registeredParameterCoarse = 0 (pitchWheelDeviation semitones)";
@@ -277,9 +283,9 @@ WebMIDI.residentSf2Synth = (function(window)
 
 			function setDataEntryCoarse(channel, semitones)
 			{
-			    checkControlExport(CTL.DATA_ENTRY_COARSE);
-			    // console.log("residentSf2Synth DataEntryCoarse: channel:" + channel + " value:" + semitones);
-			    that.dataEntryCoarse(channel, semitones);
+				checkControlExport(CTL.DATA_ENTRY_COARSE);
+				// console.log("residentSf2Synth DataEntryCoarse: channel:" + channel + " value:" + semitones);
+				that.dataEntryCoarse(channel, semitones);
 			}
 
 			checkCommandExport(CMD.CONTROL_CHANGE);
@@ -304,10 +310,10 @@ WebMIDI.residentSf2Synth = (function(window)
 				// CTL.REGISTERED_PARAMETER_FINE and CTL.DATA_ENTRY_FINE are not supported (i.e. are ignored by) this synth.
 				case CTL.REGISTERED_PARAMETER_COARSE:
 					setRegisteredParameterCoarse(channel, data2);
-			        break;
-			    case CTL.DATA_ENTRY_COARSE: // default coarse is semitones pitchWheelDeviation when RPC is 0
-			        setDataEntryCoarse(channel, data2);
-			        break;
+					break;
+				case CTL.DATA_ENTRY_COARSE: // default coarse is semitones pitchWheelDeviation when RPC is 0
+					setDataEntryCoarse(channel, data2);
+					break;
 				default:
 					console.warn(`Controller ${data1.toString(10)} (0x${data1.toString(16)}) is not supported.`);
 			}
@@ -383,19 +389,6 @@ WebMIDI.residentSf2Synth = (function(window)
 		// console.log("residentSf2Synth SoundFont set.");
 	};
 
-	// Call this immediately after the synth has been constructed.
-	ResidentSf2Synth.prototype.init = function()
-	{
-		bufSrc.connect(gainMaster);
-		gainMaster.connect(ctx.destination);
-		bufSrc.start(0);
-
-		// ji November 2015
-		this.setMasterVolume(16383);
-
-		// console.log("residentSf2Synth initialised");
-	};
-
 	ResidentSf2Synth.prototype.setMasterVolume = function(volume)
 	{
 		// masterVolume = volume; -- masterVolume unused so commented out (ji November 2015)
@@ -417,26 +410,26 @@ WebMIDI.residentSf2Synth = (function(window)
 	ResidentSf2Synth.prototype.noteOn = function(channel, key, velocity)
 	{
 		var bnkIndex = (channel === 9) ? 128 : bankIndex,
-		bank = bankSet[bnkIndex],
-		presetLayers,
-		keyLayers,
-		note,
-        midi = {},
-		pan = channelPanpot[channel] - 64,
-		bankIndexStr, instrStr, channelStr;
+			bank = bankSet[bnkIndex],
+			presetLayers,
+			keyLayers,
+			note,
+			midi = {},
+			pan = channelPanpot[channel] - 64,
+			bankIndexStr, instrStr, channelStr;
 
-	    // *Setting* the pitchBendSensitivity should be done by
-	    //   1. setting channelRegisteredParameterCoarse[channel] to 0 -- using this.registeredParameterCoarse(channel, param).
-	    //   2. setting channelDataEntryCoarse[channel] to the sensitivity (in semitones) -- using this.dataEntryCoarse(channel, semitones).
-	    // If the channelRegisteredParameterCoarse param is set !== 0, then this function will throw an exception.
-	    // These synths ignore both channelRegisteredParameterFine and channelDataEntryFine.
+		// *Setting* the pitchBendSensitivity should be done by
+		//   1. setting channelRegisteredParameterCoarse[channel] to 0 -- using this.registeredParameterCoarse(channel, param).
+		//   2. setting channelDataEntryCoarse[channel] to the sensitivity (in semitones) -- using this.dataEntryCoarse(channel, semitones).
+		// If the channelRegisteredParameterCoarse param is set !== 0, then this function will throw an exception.
+		// These synths ignore both channelRegisteredParameterFine and channelDataEntryFine.
 		function getPitchBendSensitivity(channel)
 		{
-		    if(channelRegisteredParameterCoarse[channel] !== 0)
-		    {
-		        throw "channelRegisteredParameterCoarse[channel] must be 0 to get the pitch bend sensitivity";
-		    }
-		    return channelDataEntryCoarse[channel]; // the channel's pitchBendSensitivity in semitones
+			if(channelRegisteredParameterCoarse[channel] !== 0)
+			{
+				throw "channelRegisteredParameterCoarse[channel] must be 0 to get the pitch bend sensitivity";
+			}
+			return channelDataEntryCoarse[channel]; // the channel's pitchBendSensitivity in semitones
 		}
 
 		if(bank === undefined)
@@ -540,31 +533,31 @@ WebMIDI.residentSf2Synth = (function(window)
 
 	ResidentSf2Synth.prototype.pitchBend = function(channel, lowerByte, higherByte)
 	{
-	    var pitchBend = ((lowerByte & 0x7f) | ((higherByte & 0x7f) << 7)) - 8192,
-		i, il,
-		currentNoteOn = currentNoteOns[channel];
+		var pitchBend = ((lowerByte & 0x7f) | ((higherByte & 0x7f) << 7)) - 8192,
+			i, il,
+			currentNoteOn = currentNoteOns[channel];
 
 		if(currentNoteOn !== undefined)
 		{
 			il = currentNoteOn.length;
 			for(i = 0; i < il; ++i)
 			{
-			    currentNoteOn[i].updatePlaybackRate(pitchBend);
+				currentNoteOn[i].updatePlaybackRate(pitchBend);
 			}
 		}
 		channelPitchBend[channel] = pitchBend;
 	};
 
-    // Both of these should be called by clients, but setting registeredParameterCoarse to anything other than 0
-    // will cause the retrieval of the dataEntryCoarse (pitchBendSensitivity) to throw an exception and fail.
-    // Setting registeredParameterCoarse has been suppressed in this synth's UI in the WebMIDISynthHost app. 
-	ResidentSf2Synth.prototype.registeredParameterCoarse = function (channel, param)
+	// Both of these should be called by clients, but setting registeredParameterCoarse to anything other than 0
+	// will cause the retrieval of the dataEntryCoarse (pitchBendSensitivity) to throw an exception and fail.
+	// Setting registeredParameterCoarse has been suppressed in this synth's UI in the WebMIDISynthHost app. 
+	ResidentSf2Synth.prototype.registeredParameterCoarse = function(channel, param)
 	{
-	    channelRegisteredParameterCoarse[channel] = param;
+		channelRegisteredParameterCoarse[channel] = param;
 	};
-	ResidentSf2Synth.prototype.dataEntryCoarse = function (channel, semitones)
+	ResidentSf2Synth.prototype.dataEntryCoarse = function(channel, semitones)
 	{
-	    channelDataEntryCoarse[channel] = semitones;
+		channelDataEntryCoarse[channel] = semitones;
 	};
 
 	ResidentSf2Synth.prototype.allSoundOff = function(channel)
